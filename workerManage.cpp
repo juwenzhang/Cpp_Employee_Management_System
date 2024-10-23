d143bc782b8fd79a59e7f2e2a4dde68856c69276
juwenzhang <3137536916@qq.com>
2024-10-23 11:47:55 +0800

    feat: 职工管理系统的模板的初步搭建以及管理类的实现

---
+++ workerManage.cpp
@@ -0,0 +1,12 @@
+#include "workerManage.h"
+
+// 构造函数功能的完善
+WorkerManage::WorkerManage() {
+	cout << "构造函数的实现" << endl;
+}
+
+
+// 析构函数功能的实现
+WorkerManage:: ~WorkerManage() {
+	cout << "析构函数的实现" << endl;
+}
---
+++ workerManage.h
@@ -0,0 +1,10 @@
+#pragma once  // 防止头文件重复包含
+#include<iostream>  // 包含输入输出流
+using namespace std;  // 使用标准命名空间
+
+class WorkerManage {
+public:
+	WorkerManage();  // 声明构造函数
+	
+	~WorkerManage();  // 声明析构函数
+};
